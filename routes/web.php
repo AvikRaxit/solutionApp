15ad69d39f5bdcf7eac0e5da4426bc1ab47dcaba
Avik Rakshit <39942608+avikrakshit00@users.noreply.github.com>
2023-02-28 14:10:25 +0530

    changes and add

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,19 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('login', [AuthController::class, 'index'])->name('login');
 
 
-Route::prefix('admin')->group(function () {
-    Route::get('index', [DashboardController::class, 'index'])->name('dashboard');
+
+/* Admin HR Dashboard Routes Start */
+Route::prefix('dashboard')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 });
+/* Admin HR Dashboard Routes End */
